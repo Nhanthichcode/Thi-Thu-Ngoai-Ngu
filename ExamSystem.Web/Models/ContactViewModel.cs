79558930fc7b4eaed460ead8d92cec008dd92d08
Nhanthichcode <nhanlx151@gmail.com>
2026-04-01 02:24:25 +0700

    thêm model liên lạc dành cho người dùng đến trang quản trị

---
+++ ExamSystem.Web/Models/ContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamSystem.Web.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
